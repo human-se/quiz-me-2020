90b224f4d6edfacb0bf6202d2e9e13cb1213e5c2
Scott Fleming <sdflem@gmail.com>
2020-01-25 17:52:42 -0600

    Annotating the Existing Model Class

--- app/models/mc_question.rb
+++ app/models/mc_question.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: mc_questions
+#
+#  id           :bigint           not null, primary key
+#  answer       :string
+#  distractor_1 :string
+#  distractor_2 :string
+#  question     :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class McQuestion < ApplicationRecord
 end
--- test/models/mc_question_test.rb
+++ test/models/mc_question_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: mc_questions
+#
+#  id           :bigint           not null, primary key
+#  answer       :string
+#  distractor_1 :string
+#  distractor_2 :string
+#  question     :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class McQuestionTest < ActiveSupport::TestCase
