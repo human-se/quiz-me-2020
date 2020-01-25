b8cfaca17c9e3bf76e679afa6a08816a094178c7
Scott Fleming <sdflem@gmail.com>
2020-01-24 22:13:14 -0600

    Processing User-Submitted Form Data in the Controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -19,13 +19,27 @@ class StaticPagesController < ApplicationController
 
   def contact
     respond_to do |format|
-      format.html { render :contact }
+      format.html { render :contact, locals: { feedback: {} } }
     end
   end
 
   def leave_feedback
+    required = [:name, :email, :reply, :feedback_type, :message]
+    form_complete = true
+    required.each do |f|
+      if params.has_key? f and not params[f].blank?
+        # that's good news. do nothing
+      else
+        form_complete = false
+      end
+    end
+    if form_complete
+      form_status_msg = 'Thank you for your feedback!'
+    else
+      form_status_msg = 'Please fill in all the remaining form fields and resubmit.'
+    end
     respond_to do |format|
-      format.html { render :contact }
+      format.html { render :contact, locals: { status_msg: form_status_msg, feedback: params } }
     end
   end
 
