4094ae4b0e7278565430ac5fa8494e16676e1f7c
Scott Fleming <sdflem@gmail.com>
2020-01-26 10:44:17 -0600

    Adding Seed Multiple-Choice Questions to the Database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+q1 = McQuestion.create!(
+    question: 'What does the M in MVC stand for?',
+    answer: 'Model',
+    distractor_1: 'Media',
+    distractor_2: 'Mode'
+)
+
+q2 = McQuestion.create!(
+    question: 'What does the V in MVC stand for?',
+    answer: 'View',
+    distractor_1: 'Verify',
+    distractor_2: 'Validate'
+)
+
+q3 = McQuestion.create!(
+    question: 'What does the C in MVC stand for?',
+    answer: 'Controller',
+    distractor_1: 'Create',
+    distractor_2: 'Code'
+)
