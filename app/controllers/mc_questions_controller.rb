ccc09baf49bc99dd36c6934a4c326c1790be8c2d
Scott Fleming <sdflem@gmail.com>
2020-01-31 16:25:20 -0600

    Adding an index Controller Action for McQuestion Records

--- app/controllers/mc_questions_controller.rb
+++ app/controllers/mc_questions_controller.rb
@@ -1,2 +1,10 @@
 class McQuestionsController < ApplicationController
+
+    def index
+        questions = McQuestion.all
+        respond_to do |format|
+            format.html { render :index, locals: { questions: questions } }
+        end
+    end
+
 end
