a482fd2caf20383c3d1f2f419c4ce280ba771779
Scott Fleming <sdflem@gmail.com>
2020-01-31 16:23:56 -0600

    Adding an index Route for McQuestion Records

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'about', to: 'static_pages#about', as: 'about'
   get 'contact', to: 'static_pages#contact', as: 'contact'
   post 'contact', to: 'static_pages#leave_feedback', as: 'leave_feedback'
+  get 'mc_questions', to: 'mc_questions#index', as: 'mc_questions' # index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
