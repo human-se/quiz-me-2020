547fa4dc0991b85710f5d47aea715cb573df6c9c
Scott Fleming <sdflem@gmail.com>
2020-02-01 10:52:53 -0600

    Adding a show Controller Action for McQuestion Records

--- app/controllers/mc_questions_controller.rb
+++ app/controllers/mc_questions_controller.rb
@@ -7,4 +7,11 @@ class McQuestionsController < ApplicationController
         end
     end
 
+    def show
+        question = McQuestion.find(params[:id])
+        respond_to do |format|
+            format.html { render :show, locals: { question: question } }
+        end
+    end
+
 end
