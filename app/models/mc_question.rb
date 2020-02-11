180e3edd7ad9ebc887209ce9df7a4fe31e3ae7b6
Scott Fleming <sdflem@gmail.com>
2020-02-10 20:49:27 -0600

    Creating a Custom Validation for the McQuestion Model Class

--- app/models/mc_question.rb
+++ app/models/mc_question.rb
@@ -17,4 +17,18 @@ class McQuestion < ApplicationRecord
     uniqueness: true
   validates :answer, presence: true
   validates :distractor_1, presence: true
+  validate :choices_cannot_be_duplicate
+
+  def choices_cannot_be_duplicate
+    if answer == distractor_1
+      errors.add(:distractor_1, "can't be the same as any other choice")
+    end
+    if answer == distractor_2
+      errors.add(:distractor_2, "can't be the same as any other choice")
+    end
+    if distractor_1 == distractor_2
+      errors.add(:distractor_2, "can't be the same as any other choice")
+    end
+  end
+
 end
