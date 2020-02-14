60f657bf7d8e0ae1aa719155133ce044baec298f
Scott Fleming <sdflem@gmail.com>
2020-02-14 12:13:26 -0600

    Adding the create Action for McQuestion Records

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+
   root to: redirect('/welcome', status: 302)
+
   get 'welcome', to: 'static_pages#welcome', as: 'welcome'
   get 'about', to: 'static_pages#about', as: 'about'
   get 'contact', to: 'static_pages#contact', as: 'contact'
   post 'contact', to: 'static_pages#leave_feedback', as: 'leave_feedback'
+
   get 'mc_questions', to: 'mc_questions#index', as: 'mc_questions' # index
   get 'mc_questions/new', to: 'mc_questions#new', as: 'new_mc_question' # new
+  post 'mc_questions', to: 'mc_questions#create' # create
   get 'mc_questions/:id', to: 'mc_questions#show', as: 'mc_question' # show
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
