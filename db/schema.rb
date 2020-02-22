fac270c4e4bf681a26a283bb5eebf94bc84be34c
Scott Fleming <sdflem@gmail.com>
2020-02-22 13:52:54 -0600

    Creating the Quiz Model Class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_233428) do
+ActiveRecord::Schema.define(version: 2020_02_22_195215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_01_25_233428) do
     t.string "answer"
     t.string "distractor_1"
     t.string "distractor_2"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ test/models/quiz_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: quizzes
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'test_helper'
+
+class QuizTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
