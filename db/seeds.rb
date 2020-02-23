92b5014a3628b44730323e3d1eac130830eef14e
Scott Fleming <sdflem@gmail.com>
2020-02-23 09:42:22 -0600

    Updating the McQuestion Seed Data to Use the Association

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,7 @@ quiz2 = Quiz.create!(
 )
 
 q1 = McQuestion.create!(
+    quiz: quiz1,
     question: 'What does the M in MVC stand for?',
     answer: 'Model',
     distractor_1: 'Media',
@@ -24,6 +25,7 @@ q1 = McQuestion.create!(
 )
 
 q2 = McQuestion.create!(
+    quiz: quiz1,
     question: 'What does the V in MVC stand for?',
     answer: 'View',
     distractor_1: 'Verify',
@@ -31,8 +33,33 @@ q2 = McQuestion.create!(
 )
 
 q3 = McQuestion.create!(
+    quiz: quiz1,
     question: 'What does the C in MVC stand for?',
     answer: 'Controller',
     distractor_1: 'Create',
     distractor_2: 'Code'
+)
+
+q4 = McQuestion.create!(
+    quiz: quiz2,
+    question: 'Which hash is primarily used for one way message passing from the controller to the view?',
+    answer: 'flash',
+    distractor_1: 'session',
+    distractor_2: 'params'
+)
+
+q5 = McQuestion.create!(
+    quiz: quiz2,
+    question: 'In which folder are image assets for the QuizMe app stored?',
+    answer: 'quiz-me/app/assets/images',
+    distractor_1: 'quiz-me',
+    distractor_2: 'quiz-me/images'
+)
+
+q6 = McQuestion.create!(
+    quiz: quiz2,
+    question: 'Which standard RESTful controller action is used to remove records?',
+    answer: 'destroy',
+    distractor_1: 'delete',
+    distractor_2: 'remove'
 )
