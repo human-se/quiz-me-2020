cfc57e2f57b44f48e6bc098b4d1ac912294839d4
Scott Fleming <sdflem@gmail.com>
2020-02-28 15:38:25 -0600

    Displaying the Quiz Creator on the index and show Pages for Quiz

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -4,7 +4,7 @@ class QuizzesController < ApplicationController
 
   def index
     # get all quiz objects
-    quizzes = Quiz.all
+    quizzes = Quiz.includes(:creator).all
     # display index view
     respond_to do |format|
       format.html { render :index, locals: { quizzes: quizzes } }
