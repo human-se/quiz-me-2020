5729480812fe9d18a816fb0bdbc30eec861240b4
Scott Fleming <sdflem@gmail.com>
2020-03-01 08:40:59 -0600

    Configuring the App to Use Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap'
+import { autosize } from 'autosize'
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,37 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  "window.jQuery": "jquery'",
+  "window.$": "jquery",
+  Popper: ['popper.js', 'default']
+}))
+
+environment.config.merge({
+  module: {
+    rules: [
+      {
+        test: require.resolve('jquery'),
+        use: [{
+          loader: 'expose-loader',
+          options: '$'
+        }, {
+          loader: 'expose-loader',
+          options: 'jQuery'
+        }]
+      },
+      {
+        test: require.resolve('@rails/ujs'),
+        use: [{
+          loader: 'expose-loader',
+          options: 'Rails'
+        }]
+      }
+    ]
+  }
+})
+
 module.exports = environment
